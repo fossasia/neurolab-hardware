fbaca45db85f35232fcfcab339dc169d3f5708b1
Abhi2424shek <abhi2424shekvashok@gmail.com>
2016-01-23 22:20:47 +0530

    Added My projects

---
+++ [MyApp]_[Abishek V Ashok]/app/src/main/java/com/next/www/abishekvashok/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.next.www.abishekvashok;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.coderefer.androidsplashscreenexample.R;
+
+/**
+ * Created by Abishek
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,first.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
